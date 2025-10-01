4a546a016f721f4a2d25add48104ad1279a29e11
Roald05 <rcela05@gmail.com>
2025-10-01 22:17:18 +0200

    Removed target

--- target/classes/db/migration/V5__ADD_CATEGORIES.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO store_api.categories (name)
-VALUES ('Category 1');
-INSERT INTO store_api.categories (name)
-VALUES ('Category 2');
--- target/classes/db/migration/V8__ADD_CUSTOMER_ID_FK_TO_ORDER.sql
+++
@@ -1,3 +0,0 @@
-alter table orders
-    add constraint orders_users_id_fk
-        foreign key (customer_id) references users (id);
